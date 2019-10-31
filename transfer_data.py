1971bf498ddf5795fb69835cbaff8f576275e3d5
thineth <thineth.nadarajah@gmail.com>
2019-10-31 20:15:34 +0000

    added some comments to play with local repository and commit

---
+++ transfer_data.py
@@ -0,0 +1,12 @@
+from MySQL_interface import MySQL_connect
+from GCP_interface import GCP_connect
+
+if __name__ == "__main__":
+    cursor=MySQL_connect.create_MySQLconnection()
+
+    results=GCP_connect.get_results()
+
+    MySQL_connect.insert_results(results,cursor)
+
+    # Added this comment
+    # Added 2nd comment
